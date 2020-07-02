f967db8cd8bbe46a4cdfb5c48da50e1bc27771b4
Genomu0741 <b07505019@g.ntu.edu.tw>
2020-07-03 00:25:27 +0800

    PA_socket ver-1.2

--- PA_socket/script.js
+++ PA_socket/script.js
@@ -5,30 +5,56 @@ var pairing = ["ANH","ANH","j12","ANH","j14",
                "ANH","j03","j08","j05","SPL",
                "AMP","ANH","ANH","ANH","ANH",
                "AMP","SPL","j00","j00"];
-var where = ["goCtr","goCtr","goCtr","goCtr","j14",
-             "j09","j10","j13","j06","j07",
-             "j00","j03","j08","j05","j00",
-             "j00","j00","j00","j00","j00",
-             "j00","j00","j00","j00"];
+var whereCue = ["goCtr","goCtr","goCtr","goCtr","goLeft",
+             "goCtr","goCtr","goLeft","goCsl","goCsl",
+             "j00","goCsl","goCsl","goCsl","goCtr",
+             "goCtr","goCtr","goCtr","goCtr","goCtr",
+             "goCtr","goCtr","j00","j00"];
+var orTrg = {"ANH":[0,1,3,10,16,17,18,19],"SPL":[14,21],"AMP":[15,20],"goCtr":[0,1,2,3,5,6,14,15,16,17,18,19,20,21],"goCsl":[8,9,11,12,13],"goLeft":[4,7]};
 
 $(".region-con").on('click',".jack",function(){
   var idStr = $(this).attr("id");
   var onJack = parseInt(idStr.replace("j",""))-1;
   var pairId = pairing[onJack];
+  var cueId = whereCue[onJack];
   var onPair = parseInt(pairId.replace("j",""))-1;
   if(toggleList[onJack]==0){
     $("#"+idStr).append("<div class=\"short\"></div>");
-    $("#"+pairId).children(".short").remove();
-    $("#"+pairId).append("<div class=\"short\"></div>");
+    if(pairId.charAt(0)=="j"){
+      $("#"+pairId).append("<div class=\"short\"></div>");
+    }
     toggleList[onJack]=1;
     toggleList[onPair]=1;
   }else{
     $("#"+idStr).children(".short").remove();
-    $("#"+pairId).children(".short").remove();
+    if(pairId.charAt(0)=="j"){
+      $("#"+pairId).children(".short").remove();
+    }
     toggleList[onJack]=0;
     toggleList[onPair]=0;
   }
+  orTrigger(pairId);
+  orTrigger(cueId);
 });
+var preSums = {"ANH":0,"SPL":0,"AMP":0,"goCtr":0,"goCsl":0,"goLeft":0};
+function orTrigger(trgId){
+  if(orTrg.hasOwnProperty(trgId)){
+    var trgList = orTrg[trgId];
+    var preSum = preSums[trgId];
+    var sum = 0;
+    for(i = 0; i < trgList.length; i++){
+      sum+=toggleList[trgList[i]];
+    }
+    // console.log(sum);
+    if(sum>preSum&&preSum==0){
+      $("#"+trgId).append("<div class=\"short\"></div>");
+    }else if(sum<preSum&&sum==0){
+      $("#"+trgId).children(".short").remove();
+    }
+    preSums[trgId]=sum;
+  }
+}
+
 var regionState = 0;
 $(".region-con").on('click',"#goCtr",function(){
   switch(regionState){
@@ -89,11 +115,11 @@ $(".region-con").on('click',"#goRight",function(){
   }
   regionState = 3;
 });
-
+var signposts = [{"name":"goCsl","content":"往控台區","class-name":"console"},{"name":"goCtr","content":"往控制室","class-name":"control"},{"name":"goLeft","content":"往左舞台","class-name":"leftStage"},{"name":"goRight","content":"往右舞台","class-name":"rightStage"}];
 function fromCsl2Ctr(){
   $(".region.console").animate({top:"-100vh"},2000,function(){
     $("#goCtr").remove();
-    leavingCsl();
+    leavingBtn(signposts[0]);
   });
   $(".region.control").animate({top:"50vh"},2000);
   $("#goCtr").animate({opacity: "0%"},800);
@@ -102,7 +128,7 @@ function fromCsl2Ctr(){
 function fromCtr2Csl(){
   $(".region.console").animate({top:"50%",left:"50%"},2000,function(){
     $("#goCsl").remove();
-    leavingCtr();
+    leavingBtn(signposts[1]);
   });
   $(".region.control").animate({top:"200vh"},2000);
   $("#goCsl").animate({opacity: "0%"},800);
@@ -111,7 +137,7 @@ function fromCtr2Csl(){
 function fromCsl2Left(){
   $(".region.console").animate({top:"-100vh"},2000,function(){
     $("#goLeft").remove();
-    leavingCsl();
+    leavingBtn(signposts[0]);
   });
   $(".region.leftStage").animate({left:"50%"},2000);
   $("#goLeft").animate({opacity: "0%"},800);
@@ -120,7 +146,7 @@ function fromCsl2Left(){
 function fromLeft2Csl(){
   $(".region.console").animate({top:"50%"},2000,function(){
     $("#goCsl").remove();
-    leavingLeft();
+    leavingBtn(signposts[2]);
   });
   $(".region.leftStage").animate({left:"200vw"},2000);
   $("#goCsl").animate({opacity: "0%"},800);
@@ -129,7 +155,7 @@ function fromLeft2Csl(){
 function fromCsl2Right(){
   $(".region.console").animate({top:"-100vh"},2000,function(){
     $("#goRight").remove();
-    leavingCsl();
+    leavingBtn(signposts[0]);
   });
   $(".region.rightStage").animate({left:"50%"},2000);
   $("#goRight").animate({opacity: "0%"},800);
@@ -138,7 +164,7 @@ function fromCsl2Right(){
 function fromRight2Csl(){
   $(".region.console").animate({top:"50%"},2000,function(){
     $("#goCsl").remove();
-    leavingRight();
+    leavingBtn(signposts[3]);
   });
   $(".region.rightStage").animate({left:"-100vw"},2000);
   $("#goCsl").animate({opacity: "0%"},800);
@@ -147,7 +173,7 @@ function fromRight2Csl(){
 function fromCtr2Left(){
   $(".region.leftStage").animate({left:"50%"},2000,function(){
     $("#goLeft").remove();
-    leavingCtr();
+    leavingBtn(signposts[1]);
   });
   $(".region.control").animate({top:"200vh"},2000);
   $("#goLeft").animate({opacity: "0%"},800);
@@ -156,7 +182,7 @@ function fromCtr2Left(){
 function fromCtr2Right(){
   $(".region.rightStage").animate({left:"50%"},2000,function(){
     $("#goRight").remove();
-    leavingCtr();
+    leavingBtn(signposts[1]);
   });
   $(".region.control").animate({top:"200vh"},2000);
   $("#goRight").animate({opacity: "0%"},800);
@@ -165,7 +191,7 @@ function fromCtr2Right(){
 function fromLeft2Ctr(){
   $(".region.control").animate({top:"50%"},2000,function(){
     $("#goCtr").remove();
-    leavingLeft();
+    leavingBtn(signposts[2]);
   });
   $(".region.leftStage").animate({left:"200vw"},2000);
   $("#goCtr").animate({opacity: "0%"},800);
@@ -174,7 +200,7 @@ function fromLeft2Ctr(){
 function fromLeft2Right(){
   $(".region.rightStage").animate({left:"50%"},2000,function(){
     $("#goRight").remove();
-    leavingLeft();
+    leavingBtn(signposts[2]);
   });
   $(".region.leftStage").animate({left:"200vw"},2000);
   $("#goRight").animate({opacity: "0%"},800);
@@ -183,7 +209,7 @@ function fromLeft2Right(){
 function fromRight2Left(){
   $(".region.leftStage").animate({left:"50%"},2000,function(){
     $("#goLeft").remove();
-    leavingRight();
+    leavingBtn(signposts[3]);
   });
   $(".region.rightStage").animate({left:"-100vw"},2000);
   $("#goLeft").animate({opacity: "0%"},800);
@@ -192,32 +218,14 @@ function fromRight2Left(){
 function fromRight2Ctr(){
   $(".region.control").animate({top:"50%"},2000,function(){
     $("#goCtr").remove();
-    leavingRight();
+    leavingBtn(signposts[3]);
   });
   $(".region.rightStage").animate({left:"-100vw"},2000);
   $("#goCtr").animate({opacity: "0%"},800);
 }
 
-function leavingCtr(){
-  $(".region-con").append("<div class=\"go2btn\" id=\"goCtr\">往控制室</div>");
-  $("#goCtr").css({opacity:"0%"});
-  $("#goCtr").animate({opacity: "100%"},800);
-}
-
-function leavingCsl(){
-  $(".region-con").append("<div class=\"go2btn\" id=\"goCsl\">往控台區</div>");
-  $("#goCsl").css({opacity:"0%"});
-  $("#goCsl").animate({opacity: "100%"},800);
-}
-
-function leavingRight(){
-  $(".region-con").append("<div class=\"go2btn\" id=\"goRight\">往右舞台</div>");
-  $("#goRight").css({opacity:"0%"});
-  $("#goRight").animate({opacity: "100%"},800);
-}
-
-function leavingLeft(){
-  $(".region-con").append("<div class=\"go2btn\" id=\"goLeft\">往左舞台</div>");
-  $("#goLeft").css({opacity:"0%"});
-  $("#goLeft").animate({opacity: "100%"},800);
+function leavingBtn(signspot){
+  $(".region-con").append("<div class=\"go2btn\" id=\""+signspot.name+"\">"+signspot.content+"</div>");
+  $("#"+signspot.name).css({opacity:"0%"});
+  $("#"+signspot.name).animate({opacity: "100%"},800);
 }
